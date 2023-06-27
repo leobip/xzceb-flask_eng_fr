deef4558915058432ff4fed6a8a9751c7b59f10c
leobip27 <leobip27@gmail.com>
2023-06-27 11:10:39 -0400

    init

---
+++ final_project/machinetranslation/tests/tests.py
@@ -0,0 +1,19 @@
+import unittest
+from translator import english_to_french, french_to_english
+
+
+class TestEnglishToFrench(unittest.TestCase):
+
+    def test1(self):
+
+        self.assertEqual(english_to_french('hello'), 'bonjour')
+        self.assertNotEqual(english_to_french('hello'), 'hello')
+
+class TestFrenchToEnglish(unittest.TestCase):
+
+    def test1(self):
+
+        self.assertEqual(french_to_english('bonjour'), 'hello')
+        self.assertNotEqual(english_to_french('bonjour'), 'bonjour')
+
+unittest.main()
---
+++ final_project/machinetranslation/translator.py
@@ -0,0 +1,20 @@
+'''
+Module to translate English / French
+'''
+
+from deep_translator import MyMemoryTranslator
+
+
+def english_to_french(english_text):
+    '''Function to translate English to French'''
+
+    french_text = MyMemoryTranslator(source='en', target='fr').translate(english_text)
+
+    return french_text
+
+def french_to_english(french_text):
+    '''Function to translate French to English'''
+
+    english_text = MyMemoryTranslator(source='fr', target='en').translate(french_text)
+
+    return english_text
